eecf34092f67acc7f583adfbf8d4329df4f3cc43
Ubuntu <ubuntu@ip-172-31-22-11.eu-west-1.compute.internal>
2019-08-09 17:22:22 +0000

    getElementById fixed

--- javascript/game.js
+++ javascript/game.js
@@ -13,21 +13,11 @@ list1[n] = shapes[Math.floor(Math.random() * 6)];
 for(i=0; i < list1.length; i++) {
 
     var shape = list1[i];
-    
-    console.log(shape);
-    
-    if (shape == "circle") {
-        var el = document.getElementById("circle");
-    } else if (shape == "diamond") {
-        var el = document.getElementById("diamond");
-    } else if (shape == "star") {
-        var el = document.getElementById("star");
-    } else if (shape == "heart") {
-        var el = document.getElementById("heart");
-    } else if (shape == "four_point") {
-        var el = document.getElementById("four_point");
-    } else if (shape == "triangle") {
-        var el = document.getElementById("triangle");
-    }
-    console.log(el);
+    console.log(shape)
+    var el = document.getElementById(shape);
+    el.classList.remove("off");
+    setTimeout(on, 1000);
 };
+function on() {
+    el.classList.add("off");
+}

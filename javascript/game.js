26b626180aeb26a419427aacca6d67c3f298c449
Ubuntu <ubuntu@ip-172-31-22-11.eu-west-1.compute.internal>
2019-08-09 21:46:04 +0000

    shapes flash in time

--- javascript/game.js
+++ javascript/game.js
@@ -1,23 +1,33 @@
-
 var shapes = ["circle","diamond","star","heart","four_point","triangle"];
 var list1 = new Array();
 var n = 0;
+var el = new Array();
+
+function on(element) {
+    element.classList.remove("off");
+}
+function off(element) {
+    element.classList.add("off");
+}
 
 list1[n] = shapes[Math.floor(Math.random() * 6)];
 
+n += 1;
+list1[n] = shapes[Math.floor(Math.random() * 6)];
+n += 1;
+list1[n] = shapes[Math.floor(Math.random() * 6)];
+n += 1;
+list1[n] = shapes[Math.floor(Math.random() * 6)];
+n += 1;
+list1[n] = shapes[Math.floor(Math.random() * 6)];
 n += 1;
 list1[n] = shapes[Math.floor(Math.random() * 6)];
 n += 1;
 list1[n] = shapes[Math.floor(Math.random() * 6)];
 
 for(i=0; i < list1.length; i++) {
-
-    var shape = list1[i];
-    console.log(shape)
-    var el = document.getElementById(shape);
-    el.classList.remove("off");
-    setTimeout(on, 1000);
-};
-function on() {
-    el.classList.add("off");
+    console.log(list1[i])
+    el[i] = document.getElementById(list1[i]);
+    setTimeout(on, 1500 * (i+1), el[i]);
+    setTimeout(off, 1500 * (i+1) + 1500, el[i]);
 }

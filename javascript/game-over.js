d696c6b666b6def565bced02566594ba749c3f7b
Ubuntu <ubuntu@ip-172-31-22-11.eu-west-1.compute.internal>
2019-08-11 20:40:05 +0000

    game-over css

--- javascript/game-over.js
+++ javascript/game-over.js
@@ -1,17 +1,13 @@
-/*$(document).ready(function () {
+$(document).ready(function () {
     $("#game-over").hide();
-});*/
+});
 
 function gameOver(num) {
     $("#game-over").show();
     $("#message .score").html(num);
-    
+}
 
-
-var list1 = new Array();
-var list2 = new Array();
-var n = 0;
-var m = -1;
-var el = new Array();
-var score = 0;
+function close() {
+    $("#game-over").hide();
+    console.log($("#game-over").hide());
 }
